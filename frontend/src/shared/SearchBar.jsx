26e926abe807250c3f047f7c42e70eeecf213ce7
Suyash Gupta <suyashgupta@Suyashs-MacBook-Air.local>
2025-05-11 11:08:16 +0530

    API key fix

--- frontend/src/shared/SearchBar.jsx
+++ frontend/src/shared/SearchBar.jsx
@@ -26,7 +26,7 @@ const SearchBar = ({ onContentUpdate }) => {
   const interactWithGeminiAPI = async (prompt) => {
     try {
       const response = await fetch(
-        "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=AIzaSyBGTmycbxpfyZsvSn2_vzGqU25QIRzLl-0",
+        "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=AIzaSyAqOH1aHX3CjMYQdXTjzmaUq9z2VuMjADw",
         {
           method: "POST",
           headers: {
